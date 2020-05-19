69c6d7a4babd643d2524478d1a28263fc56b5f16
YashikaWick <ywickramaarachchi.96@gmail.com>
2020-05-19 23:02:34 +0530

    product add module created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CoreComponent } from './core/core.component';
+import { ProductComponent } from './core/product/product.component';
+import { ProductAddComponent } from './core/product/product-add/product-add.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CoreComponent,
+    ProductComponent,
+    ProductAddComponent
   ],
   imports: [
     BrowserModule,
